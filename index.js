65efec33027c806a2787953e22e4d6cbc4356045
Sid Ahuja <ahuja12@illinois.edu>
2023-08-06 14:49:53 -0500

    Display 'My Library' and books inside the myLibrary array

--- index.js
+++ index.js
@@ -1,51 +1,21 @@
-let myLibrary = [];
+let myLibrary = ["Harry Potter", "Symphonies", "Where you go, there you are"];
 
-function Book() {
-  // the constructor...
-}
+// function Book(title) {
+//   this.title = title;
+// }
 
-function addBookToLibrary() {
-  var newBook = prompt('What is a book you have read recently')
-  myLibrary.push(newBook)
-}
+// function addBookToLibrary() {
+//   let newBook = new Book(prompt('What is a book you have read recently'));
+//   myLibrary.push(newBook); 
+// }
 
-myLibrary.push("the Covenant of Water");
-myLibrary.push("The Handmaid's Tale");
-myLibrary.push("Little Fires Everywhere");
-myLibrary.push("Signal Fires");
+// addBookToLibrary(); 
 
-function createBookCard(book) {
-  // const table = document.querySelector('table')
-  // table.style.width = 'auto';
-  // table.style.border = '1px solid black';
-  
-  // for (let i = 0; i < myLibrary.length; i++) {
-  // const row = table.insertRow()
-  // const cell = row.insertCell()
-  // cell.innerHTML = myLibrary[i];
-  // cell.style.border = '1px solid black';
-  // // console.log(myLibrary[i])
-  }
-}
+// document create a div
 
-createTable(myLibrary);
-
-// visibility of the form button
-const btn = document.querySelector('button')
-btn.addEventListener('click', () => {
-  const form = document.getElementById('new-book')
-  if(form.style.display === 'none') {
-    form.style.display = 'block'
-  } 
-  else {
-    form.style.display = 'none'
-  }
+myLibrary.forEach(copy => {
+  const singleBook = document.createElement('div')
+  singleBook.innerHTML = copy
+  singleBook.classList.add('single-book')
+  document.getElementById('title').after(singleBook)
 })
-
-// preventing the submit button to work
-const submit = document.querySelector(".btn[type='submit']")
-submit.addEventListener('click', (e) => {
-  e.preventDefault();
-})
-
-
